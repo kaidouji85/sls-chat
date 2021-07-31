be9a8e94bdb14d5fe749c42980caadf8d5e895e6
竹内佑介 <kaidouji85@gmail.com>
2021-07-31 20:13:53 +0900

    切断処理を追加した

--- src/disconnect.js
+++ src/disconnect.js
@@ -7,20 +7,22 @@ import {createDynamoDBClient} from "./dynamo-db/client";
 const dynamoClient = createDynamoDBClient();
 
 /**
- * $connect エントリポイント
+ * $disconnect エントリポイント
  *
  * @param event イベント
  * @return レスポンス
  */
-export async function connect(event: any): Promise<Response> {
+export async function disconnect(event: any): Promise<Response> {
   try {
     const TableName = process.env.SLS_CHAT_CONNECTIONS;
-    const Item = {connectionId: event.requestContext.connectionId};
-    await dynamoClient.put({TableName, Item}).promise();
-    return {statusCode: 200, body: 'connected.'};
+    const Key = {
+      connectionId: event.requestContext.connectionId
+    };
+    await dynamoClient.delete({TableName, Key}).promise();
+    return {statusCode: 200, body: 'disconnected'};
   } catch(err) {
     console.error(err);
-    const body = 'connection error';
+    const body = 'disconnect error';
     return {statusCode: 500, body};
   }
 }
