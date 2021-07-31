0adda9f33394865c52bb419c04715f7d9abe23be
竹内佑介 <kaidouji85@gmail.com>
2021-07-31 18:04:45 +0900

    コネクション接続時にdynamo dbにデータをセットするようにした

--- src/connect.js
+++ src/connect.js
@@ -1,8 +1,24 @@
 // @flow
 
 import type {Response} from './response';
+import {DynamoDB} from 'aws-sdk';
+
+const dbClient = new DynamoDB.DocumentClient({
+  apiVersion: '2012-08-10', region: process.env.AWS_REGION
+});
 
 export async function connect(event: any): Promise<Response> {
-  console.log('connect', event);
-  return {statusCode: 200, body: 'test'};
+  try {
+    console.log('connection');
+    const TableName = process.env.SLS_CHAT_CONNECTIONS;
+    const Item = {connectionId: event.requestContext.connectionId};
+    console.log(Item);
+    await dbClient.put({TableName, Item}).promise();
+    console.log('connection success');
+    return {statusCode: 200, body: 'Connected.'};
+  } catch(err) {
+    console.error(err);
+    const body = `Failed to connect: ${JSON.stringify(err)}`;
+    return { statusCode: 500, body};
+  }
 }
