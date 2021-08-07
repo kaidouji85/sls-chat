5525a9ed7a983817bd835bf9c96a7b4d288f4629
竹内佑介 <kaidouji85@gmail.com>
2021-08-07 22:22:44 +0900

    オーサライザにアクセストークン検証を追加した

--- src/authorizer.js
+++ src/authorizer.js
@@ -3,6 +3,10 @@
 import type {AuthorizerResponse} from "./lambda/authorizer-response";
 import {successAuthorize} from "./lambda/authorizer-response";
 import type {AuthorizerEvent} from "./lambda/event";
+import {verifyAccessToken} from "./auth0/access-token";
+
+const AUTH0_JWKS_URL = process.env.AUTH0_JWKS_URL || '';
+const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE || '';
 
 /**
  * オーサライザ
@@ -11,6 +15,9 @@ import type {AuthorizerEvent} from "./lambda/event";
  * @return 認可結果
  */
 export async function authorizer(event: AuthorizerEvent): Promise<AuthorizerResponse> {
+  console.log('authorizer');
+  const token = await verifyAccessToken(event.queryStringParameters.token, AUTH0_JWKS_URL, AUTH0_AUDIENCE);
+  console.log(token);
   const principalId = 'test';
   const resource: string = event.methodArn;
   return successAuthorize(principalId, resource);
